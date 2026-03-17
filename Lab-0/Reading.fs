22d10c2660a19769b77b6a47aa1e1cd4176337ad
Jeremy Jacob <jeremyjacob101@gmail.com>
2026-03-17 11:41:15 +0200

    [+] Added logic for first two functions and writing/printing properly

--- Lab-0/Reading.fs
+++ Lab-0/Reading.fs
@@ -3,38 +3,59 @@ module Reading
 open System
 open System.IO
 
-let getFilePath () =
-    printf "Enter file path: "
-    Console.ReadLine()
+let mutable directoryPath = ""
+let mutable currentFileName = ""
+let mutable writer: StreamWriter = null
+
+let getInputDirectoryPath () =
+    printf "Enter directory path: "
+    directoryPath <- Console.ReadLine()
+
+let getOutputFileName () = directoryPath + ".asm"
 
 let isLogicalCommand command =
     command = "eq" || command = "gt" || command = "lt"
 
-// Apply func1
-let function1 = printfn "func1"
-// Apply func2
-let function2 = printfn "func2"
-// Apply func1
-let function3 = printfn "func3"
+let handleAdd () = writer.WriteLine "command: add"
+let handleSub () = writer.WriteLine "command: sub"
+let handleNeg () = writer.WriteLine "command: neg"
+let handleEq () = writer.WriteLine "command: eq"
+let handleGt () = writer.WriteLine "command: gt"
+let handleLt () = writer.WriteLine "command: lt"
+
+let simpleCommandHandler command =
+    match command with
+    | "add" -> handleAdd ()
+    | "sub" -> handleSub ()
+    | "neg" -> handleNeg ()
+    | "eq" -> handleEq ()
+    | "gt" -> handleGt ()
+    | "lt" -> handleLt ()
+    | _ -> ()
 
 let traverseAllVmFiles () =
-    let directoryPath = getFilePath ()
+    getInputDirectoryPath ()
+
+    writer <- new StreamWriter(getOutputFileName ())
 
     Directory.GetFiles(directoryPath, "*.vm")
     |> Array.iter (fun file ->
+        currentFileName <- Path.GetFileName(file)
         let mutable counter = 0
 
         File.ReadLines file
         |> Seq.iter (fun line ->
-            let command = line.Split(' ').[0]
+            let parts = line.Split(' ')
+            let command = parts.[0]
+
+            simpleCommandHandler command
 
             if isLogicalCommand command then
                 counter <- counter + 1
+                writer.WriteLine("counter: " + string counter))
 
-            function1
-            function2
-            function3)
+        printfn "End of input file: %s" currentFileName)
 
+    writer.Close()
 
-        printfn "counter: %d" counter
-        printfn "End of input file: %s" (Path.GetFileName(file)))
+    printfn "Output file is ready: %s" (getOutputFileName ())
