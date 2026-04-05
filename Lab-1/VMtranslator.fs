1ae9b901f5916754d00f5a21a5b12578f959f1d6
Scimonster <tehalmightyscimonster@gmail.com>
2026-04-06 00:43:01 +0300

    Handle constant and static segments

--- Lab-1/VMtranslator.fs
+++ Lab-1/VMtranslator.fs
@@ -3,6 +3,8 @@ module VMtranslator
 open System
 open System.IO
 
+let mutable currentFileName = ""
+
 type Command =
     | Arithmetic of string
     | Push of string * int
@@ -12,21 +14,37 @@ let stripComments (line: string) =
     let parts = line.Split("//")
     parts[0].Trim()
 
+// Reusable assembly code snippets
 let popToD = [ "@SP"; "AM=M-1"; "D=M" ]
+let pushFromD = [ "@SP"; "M=M+1"; "A=M-1"; "M=D" ]
 let pointToTop = [ "@SP"; "A=M-1" ]
 let pointToPreviousA = [ "A=A-1" ]
 
 let writeArithmetic operation =
     match operation with
-    | "add" -> popToD @ pointToPreviousA @ [ "M=M+D" ]
+    | "add" -> popToD @ pointToPreviousA @ [ "M=D+M" ]
     | "sub" -> popToD @ pointToPreviousA @ [ "M=M-D" ]
     | "neg" -> pointToTop @ [ "M=-M" ]
     | _ -> []
 
+let getStaticVariableName index = currentFileName + "." + string index
+
+let writePush segment index =
+    match segment with
+    | "constant" -> [ "@" + string index; "D=A" ] @ pushFromD
+    | "static" -> [ "@" + getStaticVariableName index; "D=M" ] @ pushFromD
+    | _ -> [] // Handle other segments as needed
+
+let writePop segment index =
+    match segment with
+    | "static" -> popToD @ [ "@" + getStaticVariableName index; "M=D" ]
+    | _ -> [] // Handle other segments as needed
+
 let translateCommandToAsm command =
     match command with
     | Arithmetic operation -> writeArithmetic operation
-    | _ -> []
+    | Push(segment, index) -> writePush segment index
+    | Pop(segment, index) -> writePop segment index
 
 let separateCommandToParts (line: string) =
     let parts = line.Split([| ' ' |], StringSplitOptions.RemoveEmptyEntries)
@@ -38,6 +56,7 @@ let separateCommandToParts (line: string) =
 
 let translate (inputPath: string) =
     let outputPath = Path.ChangeExtension(inputPath, ".asm")
+    currentFileName <- Path.GetFileName(Path.ChangeExtension(inputPath, null))
 
     let commands =
         File.ReadAllLines inputPath
