851cb7be55f58837a76315c9349e438bd6895f9d
Jeremy Jacob <jeremyjacob101@gmail.com>
2026-06-02 11:50:21 +0300

    [+] basic tokenizer structure / jack files

---
+++ Lab-4/Tokenizer.fs
@@ -0,0 +1,55 @@
+module Tokenizer
+
+open System
+open Definitions
+
+type Token = { Kind: TokenType; Value: string }
+
+let isWhitespace = false
+let isLineComment = false
+let isBlockComment = false
+
+// Jack Lexicon
+let isSymbol (c: char) = List.contains c symbolChars
+let isDigit (c: char) = Char.IsDigit c
+let isIdentifierStart (c: char) = Char.IsLetter c || c = '_'
+let isIdentifierPart (c: char) = Char.IsLetterOrDigit c || c = '_'
+
+let skipWhitespaceAndComments (text: string) (startIndex: int) =
+    let mutable i = startIndex
+
+    while i < text.Length do
+        if isWhitespace then
+            printfn "iterate"
+
+        elif isLineComment then
+            printfn "iterate"
+
+        elif isBlockComment then
+            printfn "iterate"
+
+    i
+
+let tokenize (text: string) : Token list =
+    let mutable i = 0
+    let tokens = ResizeArray<Token>()
+
+    while i < text.Length do
+        i <- skipWhitespaceAndComments text i
+
+        if i < text.Length then
+            let c = text[i]
+
+            if isSymbol c then // Symbol
+                tokens.Add({ Kind = Keyword; Value = "" })
+
+            elif isDigit c then // Digit
+                tokens.Add({ Kind = Keyword; Value = "" })
+
+            elif c = '"' then // String
+                tokens.Add({ Kind = Keyword; Value = "" })
+
+            elif isIdentifierStart c then // Identifier/Keyword
+                tokens.Add({ Kind = Keyword; Value = "" })
+
+    List.ofSeq tokens
--- Program.fs
+++ Program.fs
@@ -1,9 +1,29 @@
 ﻿// Eyal Schachter | TZ 209792266
 // Jeremy Jacob | TZ 345570451
 
-open VMtranslator
+open System.IO
+open Tokenizer
+
+let getJackFiles path =
+    if File.Exists path then
+        [ path ]
+    else
+        Directory.GetFiles(path, "*.jack") |> Array.toList
+
+let tokenizeFile path =
+    let code = File.ReadAllText path
+    let tokens = tokenize code
+    path, tokens
 
 [<EntryPoint>]
 let main argv =
-    translate argv[0]
+    let tokenizedFiles =
+        argv |> Array.toList |> List.collect getJackFiles |> List.map tokenizeFile
+
+    for path, tokens in tokenizedFiles do
+        printfn "\n%s\n" path
+
+        for token in tokens do
+            printfn "%A %s" token.Kind token.Value
+
     0
