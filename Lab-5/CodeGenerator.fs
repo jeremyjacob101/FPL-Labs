b6777bc96308911d154989a158e405947b0e84f3
Scimonster <tehalmightyscimonster@gmail.com>
2026-06-21 12:35:24 +0300

    Expand code generation to more statements

--- Lab-5/CodeGenerator.fs
+++ Lab-5/CodeGenerator.fs
@@ -5,6 +5,13 @@ open Definitions
 
 let mutable writer: StreamWriter option = None
 
+let mutable labelCounter = 0
+let mutable currentFunctionName = "" // fully resolved function name eg Main.main
+
+let nextLabel prefix =
+    labelCounter <- labelCounter + 1
+    currentFunctionName + "$" + prefix + string labelCounter
+
 let writeData (text: string) =
     // indent all lines except for comments and function declarations for better readability of the generated VM code
     let formattedText =
@@ -82,6 +89,27 @@ and writeTerm (node: Node) =
     match node with
     | Token(IntConst, value) -> writePush CONST (int value)
     | Token(Identifier, id) -> writeData ("// push " + id) // TODO: fill in
+    | Token(Keyword, "true") ->
+        writePush CONST 1
+        writeArithmetic NEG // true is represented as -1 in Hack
+    | Token(Keyword, "false") -> writePush CONST 0
+    | Token(Keyword, "null") -> writePush CONST 0
+    | Node("unaryOpTerm", children) ->
+        writeTerm children[1]
+
+        match children[0] with
+        | Token(Symbol, "-") -> writeArithmetic NEG
+        | Token(Symbol, "~") -> writeArithmetic NOT
+        | _ -> failwithf "Unknown unary operator %A" children[0]
+    | Node("subroutineCall", children) ->
+        let functionName =
+            getChildren "subroutineCallPrefix" children[0]
+            |> List.map (getTokenValue Identifier)
+            |> String.concat "."
+
+        let expressions = getChildren "expressionList" children[1]
+        expressions |> List.iter writeExpression
+        writeCall functionName expressions.Length
     | Node("expression", _) -> writeExpression node
     | _ -> writeData (sprintf "// unhandled node %A" node)
 
@@ -91,36 +119,68 @@ and writeOp (node: Node) =
     | Token(Symbol, "-") -> writeArithmetic SUB
     | Token(Symbol, "*") -> writeCall "Math.multiply" 2
     | Token(Symbol, "/") -> writeCall "Math.divide" 2
+    | Token(Symbol, "=") -> writeArithmetic EQ
+    | Token(Symbol, ">") -> writeArithmetic GT
+    | Token(Symbol, "<") -> writeArithmetic LT
+    | Token(Symbol, "&") -> writeArithmetic AND
+    | Token(Symbol, "|") -> writeArithmetic OR
     | _ -> failwithf "Unknown operator %A" node
 
 
-let writeDoStatement (node: Node) =
-    // node should be a subroutineCall node
-    let nodes = getChildren "subroutineCall" node
+let rec writeStatement node =
 
-    let functionName =
-        getChildren "subroutineCallPrefix" nodes[0]
-        |> List.map (getTokenValue Identifier)
-        |> String.concat "."
+    let writeLetStatement (nodes: Node list) =
+        let varSegment, varIndex = (LOCAL, 0) // TODO: use symbol table
+        writeExpression nodes[1]
+        writePop varSegment varIndex
 
-    let expressions = getChildren "expressionList" nodes[1]
+    let writeIfStatement (nodes: Node list) =
+        let afterLabel = nextLabel "IF_AFTER_"
+        let elseLabel = if nodes.Length = 3 then nextLabel "ELSE_" else ""
+        let falseLabel = if elseLabel = "" then afterLabel else elseLabel
 
-    expressions |> List.iter writeExpression
+        writeExpression nodes[0] // condition
+        writeArithmetic NOT // invert the condition - no short circuiting and true/false values to track
+        writeIf falseLabel // if false, jump to else/after
+        getChildren "statements" nodes[1] |> List.iter writeStatement // true statements
 
-    writeCall functionName expressions.Length
-    // do statements discard the return value, so we pop it off the stack
-    writePop TEMP 0
+        if elseLabel <> "" then // if there is an else
+            writeGoTo afterLabel // jump to `next` after finishing the if
+            writeLabel elseLabel
+            getChildren "statements" nodes[2] |> List.iter writeStatement // else statements
 
-let writeReturnStatement (nodes: Node list) =
-    // TODO: handle return expressions
-    writePush CONST 0
-    writeReturn ()
+        writeLabel afterLabel
 
-let writeStatement node =
+    let writeWhileStatement (nodes: Node list) =
+        let loopLabel = nextLabel "LOOP_"
+        let condLabel = nextLabel "DO_COND_"
+
+        // write the `while` more like a `do-while` to avoid inverting the condition
+        writeGoTo condLabel // test condition first
+        writeLabel loopLabel
+        getChildren "statements" nodes[1] |> List.iter writeStatement // loop statements
+        writeLabel condLabel
+        writeExpression nodes[0]
+        writeIf loopLabel // jump back to top if true, fall if false
+
+    let writeDoStatement (node: Node) =
+        writeTerm node // subroutine call is also a term of an expression
+        // do statements discard the return value, so we pop it off the stack
+        writePop TEMP 0
+
+    let writeReturnStatement (nodes: Node list) =
+        if nodes.Length = 0 then
+            writePush CONST 0 // return void
+        else
+            writeExpression nodes[0] // return expression
+
+        writeReturn ()
+
+    // write statement
     match node with
-    | Node("letStatement", children) -> writeData "letStatement"
-    | Node("ifStatement", children) -> writeData "ifStatement"
-    | Node("whileStatement", children) -> writeData "whileStatement"
+    | Node("letStatement", children) -> writeLetStatement children
+    | Node("ifStatement", children) -> writeIfStatement children
+    | Node("whileStatement", children) -> writeWhileStatement children
     | Node("doStatement", children) -> writeDoStatement children[0]
     | Node("returnStatement", children) -> writeReturnStatement children
     | x -> failwithf "Unknown statement %A" x
@@ -130,6 +190,7 @@ let writeSubroutine className node =
     let subroutineType = getTokenValue Keyword children[0]
     let returnType = getTypeName children[1]
     let subroutineName = className + "." + getTokenValue Identifier children[2]
+    currentFunctionName <- subroutineName
 
     let parameterList = children[3]
 
@@ -147,11 +208,19 @@ let writeSubroutine className node =
         |> String.concat " "
     )
 
-    writeFunction subroutineName paramLength
-
     let subroutineBodyChildren = getChildren "subroutineBody" children[4]
 
     let varDecNodes = List.filter (matchNode "varDec") subroutineBodyChildren
+
+    // TODO: Maybe use symbol table for this? But this also works
+    let varDecCount =
+        varDecNodes
+        |> List.map (getChildren "varDec")
+        |> List.map List.tail
+        |> List.map List.length
+        |> List.sum
+
+    writeFunction subroutineName varDecCount
 
     let statementsNode =
         List.tryFind (matchNode "statements") subroutineBodyChildren
@@ -160,6 +229,8 @@ let writeSubroutine className node =
     let statements = getChildren "statements" statementsNode
 
     statements |> List.iter writeStatement
+
+    writeData "\n"
 
 
 let writeClass node =
